0f12a4cbac60bca380b5b0ef196f75897992a240
“erikayanez“ <erikajoam@gmail.com>
2022-08-16 15:33:24 +0200

    New Components were created

--- MyApp/movie-app/src/App.js
+++ MyApp/movie-app/src/App.js
@@ -3,35 +3,38 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from './Components/MovieList';
 import MovieListHeading from './Components/MovieListHeading';
+import SearchBox from './Components/SearchBox';
+import AddFavourites from './Components/AddFavourites';
 
-
-export default function App() {
+ const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState('');
 
         const getMovieRequest = async() => {
-          const url = "http://www.omdbapi.com/?s=avengers&apikey=1a0cfd0b";
+          const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=1a0cfd0b`;
 
           const response = await fetch(url);
           const responseJson =await response.json();
 
-          console.log(responseJson);
-          setMovies(responseJson.Search);
+          if(responseJson.Search){
+            setMovies(responseJson.Search);
+          }
         };
 
-        useEffect(()=>{
-          getMovieRequest();
-        }, []);
+        useEffect((searchValue)=>{
+          getMovieRequest(searchValue);
+        }, [searchValue]);
   return (
     <div className='container-fluid movie-app'>
-      <div className='row'>
+      <div className='row d-flex align-items-center mt-4 mb-4'>
         <MovieListHeading heading="Movies"/>
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
       </div>
       <div className='row'>
-      <MovieList movies={movies} />
+      <MovieList movies={movies} favouriteComponent={AddFavourites} />
       </div> 
     </div>
   )
 }
 
-
+export default App;
--- MyApp/movie-app/src/Components/MovieList.js
+++ MyApp/movie-app/src/Components/MovieList.js
@@ -2,11 +2,15 @@ import React from "react";
 
 
 const MovieList = (props) => {
+  const favouriteComponent = props.favouriteComponent
   return(
     <>
       {props.movies.map((movie, index)=> (
-      <div className="d-flex justify-content-flex-start m-3">
+      <div className="image-container d-flex justify-content-flex-start m-3">
         <img src={movie.Poster} alt="movie"></img>
+        <div className="overlay d-flex align-items-center justify-content-center">
+          <favouriteComponent />
+        </div>
       </div>
       ))}
     </>
